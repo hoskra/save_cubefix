1a50cb8973d07d164c34c99cd4a7fcd4553a8e97
hoskra <hoskra@gmail.com>
2023-04-16 21:29:17 +0200

    moving enemy

--- Characters/legionar.gd
+++ Characters/legionar.gd
@@ -3,37 +3,41 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+enum LEGIONAR_STATE { PATROL, ATTACK, FLEE }
+
+var health = 5
+var targeted_entity = null
+var current_state : LEGIONAR_STATE = LEGIONAR_STATE.PATROL
+var move_direction : Vector2 = Vector2.ZERO
+var previous_position : Vector2 = Vector2.ZERO
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
-	update_animation_parameters(Vector2(0,0))
+	update_animation_parameters()
 	state_machine.travel("Walk")
+	if randf_range(0, 1) > 0.5:
+		move_direction = Vector2(1, 0)
+	else:
+		move_direction = Vector2(-1, 0)
+#
+func flip_direction():
+	if move_direction.x == -1:
+		move_direction = Vector2(1, 0)
+	else:
+		move_direction = Vector2(-1, 0)
 	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity = move_direction * SPEED
 	move_and_slide()
-	update_animation_parameters(Vector2(direction, 0))
+	update_animation_parameters()
+	if (previous_position == self.position):
+		flip_direction()
+	previous_position = self.position
 
-func update_animation_parameters(move_input: Vector2):
-	if(move_input != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_input)
+func update_animation_parameters():
+	if(move_direction != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_direction)
 		
