7e6f0ac3e5245b20d2e60a004dab38bf70c5ddb5
hoskra <hoskra@gmail.com>
2023-09-10 19:31:06 +0200

    enemies with healthbars, characters can damage them; readme edit

--- Characters/legionar.gd
+++ Characters/legionar.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-enum LEGIONAR_STATE { PATROL, ATTACK, FLEE }
+enum LEGIONAR_STATE { PATROL, ATTACK, FLEE, DEAD }
 var player = null
 
 var health = 5
@@ -15,11 +15,17 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
+@onready var attack = $/root/GameLevel/Sounds/attack;
 
 var is_ready = false
 
+func set_health_bar():
+	$HealthBar.value = health
+
 func _ready():
 	update_animation_parameters()
+	set_health_bar()
+	
 	state_machine.travel("Walk")
 	if randf_range(0, 1) > 0.5:
 		move_direction = Vector2(1, 0)
@@ -35,14 +41,25 @@ func flip_direction():
 	
 func _physics_process(_delta):
 	match current_state:
+		LEGIONAR_STATE.DEAD:
+			$Sprite2D.frame = 0;
+			$CollisionShape2D.disabled = true
+			return
 		LEGIONAR_STATE.PATROL:
 			velocity = move_direction * SPEED
 			move_and_slide()
 			if (previous_position == self.position):
 				flip_direction()
 			
-			
 		LEGIONAR_STATE.ATTACK:
+			if Input.is_action_just_pressed("attack"):
+				health -= 1
+				set_health_bar()
+				attack.play()
+				if health == 0:
+					current_state = LEGIONAR_STATE.DEAD
+					await get_tree().create_timer(2).timeout
+					queue_free()
 			if ((player.position - self.position)[0]) < 0:
 				move_direction = Vector2(-1, 0)
 			else:
@@ -51,22 +68,22 @@ func _physics_process(_delta):
 			velocity = move_direction * SPEED
 			move_and_slide()
 	
-	update_animation_parameters()
+	if current_state != LEGIONAR_STATE.DEAD:
+		update_animation_parameters()
 	previous_position = self.position
 
 func update_animation_parameters():
 	if(move_direction != Vector2.ZERO):
-		animation_tree.set("parameters/Walk/blend_position", move_direction)
-		
+		animation_tree.set("parameters/Walk/blend_position", move_direction)		
 
 func _on_alert_body_entered(body):
-	if body.name == "cuberix" or body.name == "cubelix":
+	if (body.name == "cuberix" or body.name == "cubelix") and current_state != LEGIONAR_STATE.DEAD:
 		print(body.name, " in")
 		player = body
 		current_state = LEGIONAR_STATE.ATTACK 
 
 func _on_alert_body_exited(body):
-	if body.name == "cuberix" or body.name == "cubelix":
+	if (body.name == "cuberix" or body.name == "cubelix") and current_state != LEGIONAR_STATE.DEAD:
 		print(body.name, " out")
 		player = null
 		current_state = LEGIONAR_STATE.PATROL
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# save_cubefix
+# Save Cubefix!
+A Gamejam game rework in Godot Engine 4.
+
+## Godot rework
+
+> [Godot rework on itch.io.](https://icless.itch.io/save-cubefix-in-godot)
+
+### Completed
+- Demo level
+- View distance limited
+- Switch between characters
+- Enemies with healthbars
+- Player can hit enemies
+- Player can drink potions
+- Sound effects
+
+### TODO
+- Menu screen with control explanation
+- Make game winnable and loosable
+
+## The original Gamejam plan
+
+> [The Gamejam Game on itch.io.](https://icless.itch.io/save-cubefix)
+
+Why are we saving Cubefix? Cubefix has runned away because he followed the smell of a bacon that led him to the Roman teritorium. By the way, everything is a cube in this world.
+
+- Player is playing for one character at a time and he can switch between them.
+- There is a synergy if characters are close to each other.
+- A following of characters can be turned on.
+- There are roman legionaries which can be stunned by player.
+- Cubelix is the squishy character. Only him can loose health. Cuberix has to save him.
+
+### Cubelix
+- can block enemy attack, but can't attack
+- his ability: sees far away
+- when potion is active: attack do stun enemy
+
+### Cuberix
+- can block enemy attack, but can't attack
+- his ability: attack stuns enemy
+- when potion is active: breaks walls
+
+### AI - Legionary
+- attack Cubelix
+- run away from Cuberix
+
+### Obsticle
+- Light (wooden) - can break through
+- Hard (stone) - can break through only with potion active
+
+
